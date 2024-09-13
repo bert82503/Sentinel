5b0c0feebfe5711feeaeb5b8ad91f1ce6ef041c9
bert82503 <bert825_work@163.com>
2024-09-13 12:23:38 +0800

    Improve DynamicRuleApi and remove bean flowRuleDefaultProvider, flowRuleDefaultPublisher

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/DynamicRuleApi.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/DynamicRuleApi.java
@@ -19,7 +19,7 @@ public interface DynamicRuleApi<T extends RuleEntity>
      * @return 规则实体的泛化类型
      */
     @SuppressWarnings("unchecked")
-    default Class<T> getRuleEntityClass() {
+    default Class<T> getRuleEntityType() {
         // 获取泛型类型T的Class对象
         ParameterizedType genericSuperclass = (ParameterizedType) getClass().getGenericSuperclass();
         if (genericSuperclass == null) {
@@ -31,6 +31,32 @@ public interface DynamicRuleApi<T extends RuleEntity>
             Type typeArgument = actualTypeArguments[0];
             if (typeArgument instanceof Class<?>) {
                 return (Class<T>) typeArgument;
+            } else {
+                throw new IllegalArgumentException("Type argument is not a Class: " + typeArgument);
+            }
+        }
+
+        throw new IllegalArgumentException("Generic superclass not parameterized");
+    }
+
+    /**
+     * 返回规则实体的泛化类型。
+     *
+     * @return 规则实体的泛化类型
+     */
+    @SuppressWarnings("unchecked")
+    default Type getRuleEntityListType() {
+        // 获取泛型类型T的Class对象
+        ParameterizedType genericSuperclassInterface = (ParameterizedType) (getClass().getSuperclass().getInterfaces()[0].getGenericInterfaces()[0]);
+        if (genericSuperclassInterface == null) {
+            throw new IllegalArgumentException("Generic superclass interface not parameterized");
+        }
+
+        Type[] actualTypeArguments = genericSuperclassInterface.getActualTypeArguments();
+        if (actualTypeArguments.length > 0) {
+            Type typeArgument = ((ParameterizedType) actualTypeArguments[0]).getRawType();
+            if (typeArgument instanceof Class<?>) {
+                return typeArgument;
             } else {
                 throw new IllegalArgumentException("Type argument is not a Class: " + typeArgument);
             }
